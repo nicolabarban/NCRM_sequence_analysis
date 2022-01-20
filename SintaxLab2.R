8c8d85cf588080d0b9469fc2e312ba93ffedf435
nicolabarban <nicola.barban@gmail.com>
2022-01-20 18:25:45 +0100

    aa

--- SintaxLab2.R
+++ SintaxLab2.R
@@ -18,8 +18,8 @@ mvad.labels=c("employment","further education",
 mvad.scode=c("EM","FE", "HE", "JL", "SC", "TR")
 
 ####################################
-#create sequence object
-mvad.seq=seqdef(mvad,17:86,states=mvad.scode, labels=mvad.labels)
+#create sequence object WEIGHTED
+mvad.seq=seqdef(mvad,17:86,states=mvad.scode, labels=mvad.labels, weights=mvad$weight )
 alphabet(mvad.seq)
 stlab(mvad.seq)
 help(seqdef)
@@ -33,8 +33,8 @@ seqtab(mvad.seq)
 # transversal statistics
 seqstatd(mvad.seq[, 1:8])
 
-# number of transitions
-seqtransn(mvad.seq[1:10,]) 
+# duration list
+duration<-seqistatd(mvad.seq)
 
 ## transition matrix
 mvad.trate = seqtrate(mvad.seq) 
@@ -51,6 +51,7 @@ par(mfrow=c(1,2))
   Turbulence=seqST(mvad.seq)
   hist(Turbulence, col="grey", main="Histogram of sequence turbulence")
 }
+par(mfrow=c(1,1))
 
 ###plot longitudinal entropy
 
@@ -86,9 +87,11 @@ mvad.cl5[1:10]
 
 seqdplot(mvad.seq, group=mvad.cl5, border=NA, space=0)
 levels(mvad.cl5)=c(" HE dominated", "FE dominated", "Employment Dominated", "Long  training", "Joblessness dominated")
-
-
 seqdplot(mvad.seq, group=mvad.cl5, border=NA, space=0)
+
+
+
+
 
 ###medoids
 dc= disscenter(dist.om1, group= mvad.cl5, medoids.index="first")
@@ -110,5 +113,66 @@ summary(jobless.reglog)
 
 library(foreign)
 write.dta(mvad, file="mvad_cluster.dta")
+
+##### Multichannel sequence analysis
+data(biofam)
+
+## Building one channel per type of event left, children or married
+bf <- as.matrix(biofam[, 10:25])
+children <-  bf==4 | bf==5 | bf==6
+married <- bf == 2 | bf== 3 | bf==6
+left <- bf==1 | bf==3 | bf==5 | bf==6
+
+## Building sequence objects
+child.seq <- seqdef(children)
+marr.seq <- seqdef(married)
+left.seq <- seqdef(left)
+
+## Using transition rates to compute substitution costs on each channel
+mcdist <- seqdistmc(channels=list(child.seq, marr.seq, left.seq),
+  method="OM", sm =list("TRATE", "TRATE", "TRATE"))
+
+## Using a weight of 2 for children channel and specifying substitution-cost
+smatrix <- list()
+smatrix[[1]] <- seqsubm(child.seq, method="CONSTANT")
+smatrix[[2]] <- seqsubm(marr.seq, method="CONSTANT")
+smatrix[[3]] <- seqsubm(left.seq, method="TRATE")
+mcdist2 <- seqdistmc(channels=list(child.seq, marr.seq, left.seq),
+  method="OM", sm =smatrix, cweight=c(2,1,1)) 
+
+
+####### Measuring alternative cluser solutions
+
+mvad.cl4 <- cutree(mvad.clusterward, k = 4)
+mvad.cl6 <- cutree(mvad.clusterward, k = 6)
+seqdplot(mvad.seq, group=mvad.cl5, border=NA, space=0)
+
+seqdplot(mvad.seq, group=mvad.cl4, border=NA, space=0)
+seqdplot(mvad.seq, group=mvad.cl6, border=NA, space=0)
+
+install.packages(WeightedCluster)
+library(WeightedCluster)
+####### Statistics for cluster solution
+qual<-list()
+
+R2<-c()
+ASW<-c()
+
+for(j in 2:10){
+  qual[[j]] <- wcClusterQuality(dist.om1, cutree(dist.ward, k = j), weights=mvad$weights)
+  R2[j]<-qual[[j]]$stats[7]
+  ASW[j]<-qual[[j]]$stats[4]
+
+}
+
+for(j in 2:10){
+    ASW[j]<-qual[[j]]$stats[4]
+}
+par(mfrow=c(1,2))
+plot(1:10,ASW, type="l")
+
+wardRange <- as.clustrange(mvad.clusterward, diss = dist.om1, ncluster = 10, weights=mvad$weights)
+summary(wardRange, max.rank = 2)
+plot(wardRange, stat = c("ASW", "HG", "PBC", "HC"))
 
 
