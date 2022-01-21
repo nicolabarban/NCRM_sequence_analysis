544d73a4aaa6b84b4f0ba0bdff80028daa40f54d
nicolabarban <nicola.barban@gmail.com>
2022-01-21 12:34:54 +0100

    ddd

--- SintaxLab2.R
+++ SintaxLab2.R
@@ -8,7 +8,7 @@
 
 #### loading traminer package. 
 library(TraMineR)
-
+data(mvad)
 
 #add the labels to be used in the graphics
 mvad.labels=c("employment","further education", 
@@ -19,7 +19,8 @@ mvad.scode=c("EM","FE", "HE", "JL", "SC", "TR")
 
 ####################################
 #create sequence object WEIGHTED
-mvad.seq=seqdef(mvad,17:86,states=mvad.scode, labels=mvad.labels, weights=mvad$weight )
+mvad.seq=seqdef(mvad,15:86,states=mvad.scode, labels=mvad.labels, weights=mvad$weight )
+
 alphabet(mvad.seq)
 stlab(mvad.seq)
 help(seqdef)
@@ -31,36 +32,58 @@ help(seqdef)
 seqtab(mvad.seq)
 
 # transversal statistics
-seqstatd(mvad.seq[, 1:8])
+
 
 # duration list
-duration<-seqistatd(mvad.seq)
+mvad.duration<-seqistatd(mvad.seq)
+mvad.duration
+
+EM.duration<-mvad.duration[,1]
+SC.duration<-mvad.duration[,5]
+JL.duration<-mvad.duration[,4]
+
+
+
+seqIplot(mvad.seq)
+
+seqIplot(mvad.seq, sortv=EM.duration)
+seqIplot(mvad.seq, sortv=-EM.duration)
+
+seqIplot(mvad.seq, sortv=c(EM.duration,SC.duration,JL.duration))
+
+
+
 
 ## transition matrix
 mvad.trate = seqtrate(mvad.seq) 
 round(mvad.trate, 2)
 
 
-par(mfrow=c(1,2))
-{
+
   #entropy of state distribution
   Entropy= seqstatd(mvad.seq)$Entropy
   plot(Entropy,main="Entropy of the state distribution ",
        col="black", xlab="Time in months", ylab="Entropy", type="l")
+
+
   #sequence turbulence
   Turbulence=seqST(mvad.seq)
   hist(Turbulence, col="grey", main="Histogram of sequence turbulence")
-}
-par(mfrow=c(1,1))
+seqIplot(mvad.seq, sortv=Turbulence)
 
-###plot longitudinal entropy
 
 
 ####################################
 # Compute Optimal Matching with transition rates
 ####################################
-submat=seqsubm(mvad.seq, method="TRATE")
-dist.om1=seqdist(mvad.seq, method="OM", indel=1, sm=submat)
+
+trcost <- seqcost(mvad.seq, method="TRATE")
+
+dist.om1=seqdist(mvad.seq, method="OM", indel=trcost$indel, sm=trcost$sm)
+
+
+
+
 
 dist.om1[1:10,1:10]
 
@@ -75,37 +98,49 @@ mvad.clusterward = agnes(dist.om1, diss = T, method = "ward")
 plot(mvad.clusterward, ask = F, which.plots = 2)
 
 ### Choose the number of clusters
-mvad.cl5 = cutree(mvad.clusterward, k = 5)
-mvad.cl5[1:10]
+mvad.cl4 = cutree(mvad.clusterward, k = 4)
+mvad.cl4[1:10]
 
-## Cluster 1 HE dominated
-## Cluster 2 FE dominated
-## Cluster 3 Employment Dominated
+
+## Cluster 1 EM dominated
+## Cluster 2 HE dominated
+## Cluster 3 FE Dominated
 ## Cluster 4 Long  training
-## Cluster 5 Joblessness dominated
+## Cluster 5 HE dominated
 ###########
 
+seqdplot(mvad.seq, group=mvad.cl4, border=NA, space=0)
+levels(mvad.cl4)=c(" EM dominated", "HE dominated", "FE dominated", "Joblessness dominated")
+seqdplot(mvad.seq, group=mvad.cl4, border=NA, space=0, main=levels(mvad.cl4))
+
+#### Trying different cluster solution
+
+####### Measuring alternative cluser solutions
+
+mvad.cl5 <- cutree(mvad.clusterward, k = 5)
+mvad.cl3 <- cutree(mvad.clusterward, k = 3)
+seqdplot(mvad.seq, group=mvad.cl4, border=NA, space=0)
+dev.new()
+seqdplot(mvad.seq, group=mvad.cl3, border=NA, space=0)
+dev.new()
+
 seqdplot(mvad.seq, group=mvad.cl5, border=NA, space=0)
-levels(mvad.cl5)=c(" HE dominated", "FE dominated", "Employment Dominated", "Long  training", "Joblessness dominated")
-seqdplot(mvad.seq, group=mvad.cl5, border=NA, space=0)
-
-
-
-
 
 ###medoids
-dc= disscenter(dist.om1, group= mvad.cl5, medoids.index="first")
+dc= disscenter(dist.om1, group= mvad.cl4, medoids.index="first")
 print(mvad.seq[dc,], format = "SPS")
 
+
+
 ############ Tables
-seqtab(mvad.seq[mvad.cl5==1,])
-seqtab(mvad.seq[mvad.cl5==2,])
+seqtab(mvad.seq[mvad.cl4==1,])
+seqtab(mvad.seq[mvad.cl4==2,])
 
 
 
 #######logistic regression joblesseness
 
-jobless = mvad.cl5 == 5
+jobless = mvad.cl4 == 4
 jobless.reglog = glm(jobless ~ male + funemp + gcse5eq, family = binomial(link = logit),data = mvad)
 summary(jobless.reglog)
 
@@ -114,6 +149,7 @@ summary(jobless.reglog)
 library(foreign)
 write.dta(mvad, file="mvad_cluster.dta")
 
+################################################
 ##### Multichannel sequence analysis
 data(biofam)
 
@@ -132,47 +168,27 @@ left.seq <- seqdef(left)
 mcdist <- seqdistmc(channels=list(child.seq, marr.seq, left.seq),
   method="OM", sm =list("TRATE", "TRATE", "TRATE"))
 
+
+
 ## Using a weight of 2 for children channel and specifying substitution-cost
 smatrix <- list()
 smatrix[[1]] <- seqsubm(child.seq, method="CONSTANT")
 smatrix[[2]] <- seqsubm(marr.seq, method="CONSTANT")
 smatrix[[3]] <- seqsubm(left.seq, method="TRATE")
+
 mcdist2 <- seqdistmc(channels=list(child.seq, marr.seq, left.seq),
   method="OM", sm =smatrix, cweight=c(2,1,1)) 
 
 
-####### Measuring alternative cluser solutions
+biofam.clusterward = agnes(mcdist, diss = T, method = "ward") 
+plot(biofam.clusterward, ask = F, which.plots = 2)
+biofam.cl4 <- cutree(biofam.clusterward, k = 4)
 
-mvad.cl4 <- cutree(mvad.clusterward, k = 4)
-mvad.cl6 <- cutree(mvad.clusterward, k = 6)
-seqdplot(mvad.seq, group=mvad.cl5, border=NA, space=0)
+########
+seqdplot(child.seq, group=biofam.cl4)
+seqdplot(marr.seq, group=biofam.cl4)
+seqdplot(left.seq, group=biofam.cl4)
 
-seqdplot(mvad.seq, group=mvad.cl4, border=NA, space=0)
-seqdplot(mvad.seq, group=mvad.cl6, border=NA, space=0)
 
-install.packages(WeightedCluster)
-library(WeightedCluster)
-####### Statistics for cluster solution
-qual<-list()
-
-R2<-c()
-ASW<-c()
-
-for(j in 2:10){
-  qual[[j]] <- wcClusterQuality(dist.om1, cutree(dist.ward, k = j), weights=mvad$weights)
-  R2[j]<-qual[[j]]$stats[7]
-  ASW[j]<-qual[[j]]$stats[4]
-
-}
-
-for(j in 2:10){
-    ASW[j]<-qual[[j]]$stats[4]
-}
-par(mfrow=c(1,2))
-plot(1:10,ASW, type="l")
-
-wardRange <- as.clustrange(mvad.clusterward, diss = dist.om1, ncluster = 10, weights=mvad$weights)
-summary(wardRange, max.rank = 2)
-plot(wardRange, stat = c("ASW", "HG", "PBC", "HC"))
-
+#########
 
